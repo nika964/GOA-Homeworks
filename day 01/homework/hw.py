e03d3eb21e90724743e177e1242df948a16bd678
nika964 <njafaridze774@gmail.com>
2025-01-25 03:26:15 -0800

    first commit

---
+++ day 01/homework/hw.py
@@ -0,0 +1,10 @@
+name = "nika"
+surname = "jafaridze"
+age = "14"
+born = "2010"
+birthday = "31_december"
+grade = "8"
+height = '170.2'
+hobby = "football"
+
+print(name +" "+ surname +" "+ age +" "+ born +" "+ birthday +" "+ grade +" "+ height +" "+ hobby)
