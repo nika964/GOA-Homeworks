a5be17309bebd5a7f8ea3a8f01b50bef2c101d4c
nika964 <njafaridze@gmail.com>
2025-02-24 10:29:28 -0800

    first commit

--- day 02/homework/hw0.py
+++ day 02/homework/hw0.py
@@ -1,24 +1,8 @@
+#გააკეთეთ ორ რიცხვს შორის თითოეულ მათემატიკურ ოპერაციაზე 5-5 მაგალითი (+,-,*,/) და დაპრინტეთ
+
 print(5 + 5)
 print(5 - 5)
 print(5 * 5)
 print(10 / 5)
  
 
-name = "nika"
-surname = "jafaridze"
-age = "14"
-
-print(name +" "+ surname +" "+ age)
-
-
-name = "nika" #str
-age = 14 #int
-height = 170.2 #float
-
-name = "luka" #str
-age = 12 #int
-height = 162.2 #float
-
-name = "vaxo" #str
-age = 9 #int
-height = 142.3 #float
---
+++ day 02/homework/hw3.py
@@ -0,0 +1,27 @@
+#შექმენი 9 ცვლადი, 3 ცვლადი იყოს Str, 3 ცვლადი int, 3 ცვლადი float, და შემდეგ გამოიტანე ტერმინალში 9-ვე ცვლადი print-ის მეშვეობით.
+#string ტიპის ცვლადები
+name_1 = "nika" 
+name_2 = "luka"
+name_3 = "vaxo"
+
+print(name_1)
+print(name_2)
+print(name_3)
+
+#integer ტიპის ცვლადები
+age_1 = 14
+age_2 = 12
+age_3 = 9
+
+print(age_1)
+print(age_2)
+print(age_3)
+
+#float ტიპის ცვლადები
+height_1 = 160.5
+height_2 = 154.3
+height_3 = 143.4
+
+print(height_1)
+print(height_2)
+print(height_3)
