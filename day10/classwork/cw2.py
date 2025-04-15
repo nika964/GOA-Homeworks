04910aee8858aeb609e02024a83033398ea95e24
nika964 <njafaridze@gmail.com>
2025-04-15 02:06:05 -0700

    first commit

---
+++ day10/classwork/cw2.py
@@ -0,0 +1,4 @@
+num = int(input("first number: "))
+num_1 = int(input("second number: "))
+
+print(num > num_1)
---
+++ day10/homework/hw1.py
@@ -0,0 +1,6 @@
+#მომხმარებელს შემოატანინეთ მისი სახელი და გაიგეთ უდრის თუ არა ის თქვენს სახელს
+
+user = input("enter your name: ")
+me = input("my name: ")
+
+print (user == me)
