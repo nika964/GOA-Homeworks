45d0cd4448bdf9ba23e18e87bb9fd56241ee339f
nika964 <njafaridze@gmail.com>
2025-06-25 18:30:15 +0400

    first commit

---
+++ day18/homework/hw3.py
@@ -0,0 +1,12 @@
+# სია, სადაც თითოეული ტიპი ერთხელაა
+data_types = [
+    10,            
+    "apple",       
+    3.14,          
+    True           
+]
+
+
+string_value = data_types[-3]  
+
+print("String მნიშვნელობა უარყოფითი ინდექსით:", string_value)
